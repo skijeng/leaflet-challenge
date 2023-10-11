61a220c8feeff3785d15dd0137794357ba3f4ef5
skijeng <133812636+skijeng@users.noreply.github.com>
2023-10-11 09:44:36 -0600

    Update logic.js

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,6 +1,6 @@
 // Store our API endpoint as a queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
-// Perform a GET request to the query URL/
+// Perform a GET request to the query URL
 d3.json(queryUrl).then(function (data) {
   createFeatures(data.features);
 });
@@ -64,9 +64,8 @@ function createMap(earthquakes) {
     layers: [topo, earthquakes]
   });
 
-  // Create a layer control.
+  // Create a layer control and add to the map.
   // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
